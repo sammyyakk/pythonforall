9b9889c6942a48bbc2e08d0d272e690ded9b0e25
VaibDeveloper <kvbhav2005@gmail.com>
2024-10-14 03:16:33 +0530

    Fixed Another Small Error :)

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -19,7 +19,7 @@ export default function Navbar() {
     <header className="sticky top-0 z-[100] w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 items-center justify-between md:justify-start">
         <Link className="flex items-center justify-center" href="/">
-          <Image src="./pfa_logo.ico" className="h-9 w-9" alt="Logo"/>
+          <Image src="/pfa_logo.ico" width={32} height={32} alt="Logo"/>
           <span className="ml-0.5 text-lg font-bold">PythonForAll</span>
         </Link>
 
